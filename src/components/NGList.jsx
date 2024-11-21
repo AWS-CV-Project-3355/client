b37002224427b076c74c4bed1300d9365731a510
kimjihyun <kimjihyun@MacBook-Pro.local>
2024-11-21 21:54:37 +0900

    feat: add nglist detail (#8)

--- src/components/NGList.jsx
+++ src/components/NGList.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import '../assets/css/NGList.css'; // 스타일을 정의할 CSS 파일
+import '../assets/css/NGList.css';
 
-const NGList = () => {
+const NGList = ({ onItemClick }) => {
     const data = [
-        { id: 1, image: "/path/to/image1.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
-        { id: 2, image: "/path/to/image2.png", status: "NG", camera: "대각선 오른쪽 위", description: "ng-뜯김" },
-        { id: 3, image: "/path/to/image3.png", status: "NG", camera: "대각선 왼쪽 아래", description: "ng-뜯김" },
-        { id: 1, image: "/path/to/image1.png", status: "NG", camera: "대각선 오른쪽 아래", description: "ng-뜯김" },
-        { id: 2, image: "/path/to/image2.png", status: "NG", camera: "정면", description: "ng-뜯김" },
-        { id: 3, image: "/path/to/image3.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
-        { id: 1, image: "/path/to/image1.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
-        { id: 2, image: "/path/to/image2.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
-        { id: 3, image: "/path/to/image3.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
+        { id: 1, image: "/path/to/image1.png", status: "NG", camera: "대각선 왼쪽 위", description: "1뜯김" },
+        { id: 2, image: "/path/to/image2.png", status: "NG", camera: "대각선 오른쪽 위", description: "2뜯김" },
+        { id: 3, image: "/path/to/image3.png", status: "NG", camera: "대각선 왼쪽 아래", description: "3뜯김" },
+        { id: 4, image: "/path/to/image1.png", status: "NG", camera: "대각선 오른쪽 아래", description: "4뜯김" },
+        { id: 5, image: "/path/to/image2.png", status: "NG", camera: "정면", description: "ng-뜯김" },
+        { id: 6, image: "/path/to/image3.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
+        { id: 7, image: "/path/to/image1.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
+        { id: 8, image: "/path/to/image2.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
+        { id: 9, image: "/path/to/image3.png", status: "NG", camera: "대각선 왼쪽 위", description: "ng-뜯김" },
     ];
 
     return (
         <div className="ng-list">
             {data.map((item) => (
-                <div className="ng-item" key={item.id}>
+                <div
+                    className="ng-item"
+                    key={item.id}
+                    onClick={() => onItemClick(item.id)}
+                >
                     <img src={item.image} className="ng-image" />
                     <span className="ng-description">{item.description}</span>
                     <span className="ng-camera">{item.camera}</span>
